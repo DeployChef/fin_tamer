f5d114b3f6642a83d488324b9da46459fd89b32d
DeployChef <dc.titovvlad@gmail.com>
2025-06-09 12:35:38 +0300

    add freezed account

---
+++ lib/features/account/model/account.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'account.freezed.dart';
+part 'account.g.dart';
+
+@freezed
+abstract class Account with _$Account {
+  const Account._();
+
+  const factory Account({
+    required int id,
+    required int userId,
+    required String name,
+    required String balance,
+    required String currency,
+    required String createdAt,
+    required String updatedAt,
+  }) = _Account;
+
+  factory Account.fromJson(Map<String, dynamic> json) => _$AccountFromJson(json);
+}
