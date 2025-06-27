43b5e3cb71ba0f71ab866708984626af58b8cb64
DeployChef <dc.titovvlad@gmail.com>
2025-06-27 13:07:14 +0300

    refactoring theme

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryLight = Color(0xff2AE881);
+  static const Color backgroundLight = Color(0xffFEF7FF);
   static const Color navigationLight = Color(0xffD4FAE6);
   static const Color foregroundLight = Color(0xff1D1B20);
   static const Color alarmLight = Color(0xffE46962);
---
+++ lib/styles/app_fonts.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const TextStyle titleLarge = TextStyle(
+    fontSize: 22,
+    height: 28 / 22,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle labelMedium = TextStyle(
+    fontSize: 12,
+    height: 16 / 12,
+    fontWeight: FontWeight.w600,
+    letterSpacing: 0.5,
+  );
+
+  static const TextStyle bodyLarge = TextStyle(
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  );
+
+  static const TextStyle headlineMedium = TextStyle(
+    fontSize: 28,
+    height: 36 / 28,
+    letterSpacing: 0,
+  );
+}
