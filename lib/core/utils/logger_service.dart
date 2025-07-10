f514a8dcf589ef45909b72d8d45ebc7f0678417e
DeployChef <dc.titovvlad@gmail.com>
2025-07-10 14:19:59 +0300

    fix logging

--- lib/core/utils/logger_service.dart
+++ lib/core/utils/logger_service.dart
@@ -4,25 +4,11 @@ import 'dart:convert'; // Added for JsonEncoder
 
 // Новый форматтер логов с поддержкой тегов, pretty-print и структурой
 class StructuredLogPrinter extends LogPrinter {
-  static const _reset = '\x1B[0m';
-  static const _gray = '\x1B[90m';
-  static const _blue = '\x1B[34m';
-  static const _yellow = '\x1B[33m';
-  static const _red = '\x1B[31m';
-  static const _green = '\x1B[32m';
-
   static final Map<Level, String> _levelPrefixes = {
     Level.debug: 'DBG',
     Level.info: 'INF',
     Level.warning: 'WRN',
     Level.error: 'ERR',
-  };
-
-  static final Map<Level, String> _levelColors = {
-    Level.debug: _blue,
-    Level.info: _gray,
-    Level.warning: _yellow,
-    Level.error: _red,
   };
 
   static const int _maxMsgLength = 500;
@@ -45,25 +31,25 @@ class StructuredLogPrinter extends LogPrinter {
         .substring(0, 19);
     final level =
         _levelPrefixes[event.level] ?? event.level.toString().toUpperCase();
-    final color = _levelColors[event.level] ?? _reset;
     String tag = '';
     String msg = event.message;
-    // Попытка вытащить тег из сообщения вида [TAG] message
     final tagMatch = RegExp(r'^\[(.*?)\]\s?(.*)').firstMatch(msg);
     if (tagMatch != null) {
       tag = '[${tagMatch.group(1)}]';
       msg = tagMatch.group(2) ?? '';
     }
-    // Pretty print для объектов
     if (msg.length > _maxMsgLength) {
       msg = msg.substring(0, _maxMsgLength) + '... (truncated)';
     }
-    // Ошибка и stacktrace
     final error =
         event.error != null ? '\n  Exception: ${_pretty(event.error)}' : '';
     final stack =
         event.stackTrace != null ? '\n  Stack: ${event.stackTrace}' : '';
-    return ['$color[$time][$level]$tag$_reset $msg$error$stack'];
+    // Plain text, без цветов, с разделителем
+    return [
+      '---',
+      '[$time][$level]$tag $msg$error$stack',
+    ];
   }
 }
 
